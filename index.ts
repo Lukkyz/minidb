ceff511dd43ac7e1f6bdafb3330d8c059973b79c
lucas <lucas@desktop.home>
2024-05-25 19:34:07 +0200

    Implement updateMany

--- index.ts
+++ index.ts
@@ -7,28 +7,23 @@ let users = new Schema("User", {
 });
 await users.init();
 
-// await users.create({
-//   username: "A",
-//   email: "bienaimelucas@hotmail.fr",
-// });
-//
-// await users.create({
-//   username: "B",
-//   email: "bienaimelucas@hotmail.fr",
-// });
-//
-
-const updated = await users.update({
-  id: 2,
-  username: "lol",
-  email: "abcdef@email.fr",
-});
-
 const begin = performance.now();
-const test = await users.data.find((user: any) => user.id === 36);
-console.log(test);
-console.log(performance.now() - begin);
 
-const begint = performance.now();
-console.log(users.data[35]);
-console.log(performance.now() - begint);
+const data = [];
+
+for (let i = 0; i < 10000; i++) {
+  data.push({
+    username: "Bob",
+    email: "bob@bob.com",
+  });
+}
+
+await users.createMany(data);
+
+await users.updateMany(
+  {
+    username: "Bob",
+  },
+  { username: "Alice" },
+);
+console.log(performance.now() - begin);
--- minidb.ts
+++ minidb.ts
@@ -2,7 +2,7 @@ import { zodToJsonSchema } from "zod-to-json-schema";
 import { z, ZodError } from "zod";
 
 export class Schema {
-  filename: string = "";
+  filename: string;
   type: any = {};
   schema: any = undefined;
   data: any = [];
@@ -23,7 +23,7 @@ export class Schema {
     }
   }
 
-  async write(): Promise<void> {
+  async #write(): Promise<void> {
     await Bun.write(
       this.filename,
       JSON.stringify({
@@ -38,23 +38,49 @@ export class Schema {
     try {
       this.type.parse(newObject);
       this.data.push(newObject);
-      await this.write();
+      await this.#write();
     } catch (e) {
       return e;
     }
   }
 
-  findById(id: number): void {
-    return this.data[id - 1];
+  async createMany(newObjects: any[]): Promise<any | ZodError> {
+    for await (const newObj of newObjects) {
+      newObj.id = this.data.length;
+      try {
+        this.type.parse(newObj);
+        this.data.push(newObj);
+      } catch (e) {
+        return e;
+      }
+    }
+    await this.#write();
+  }
+
+  findById(id: number): any {
+    return this.data.find((element: any) => element.id === id);
   }
 
   async update(obj: any): Promise<any | ZodError> {
     try {
       this.data[obj.id] = this.type.parse(obj);
-      await this.write();
+      await this.#write();
       return this.data[obj.id];
     } catch (e) {
       return e;
     }
+  }
+
+  async updateMany(filter: any, updatedObject: any): Promise<any | ZodError> {
+    this.data.forEach((element: any, i: number) => {
+      let match: boolean = true;
+      for (const k of Object.keys(filter)) {
+        match = match && filter[k] == element[k];
+      }
+      if (match) {
+        this.data[i] = { ...this.data[i], ...updatedObject };
+      }
+    });
+    await this.#write();
   }
 }
